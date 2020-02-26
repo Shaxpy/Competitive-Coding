b2410d471734a6fd05b3f973fe072ececb7e2cbf
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2020-02-27 00:48:23 +0530

    Update Min Absolute difference in an array.cpp

--- Algorithms Warmup/Greedy Algo/Min Absolute difference in an array.cpp
+++ Algorithms Warmup/Greedy Algo/Min Absolute difference in an array.cpp
@@ -1,20 +1,27 @@
-#include <bits/stdc++.h>
+
+#include <cmath>
+#include <limits>
+#include <cstdio>
 #include <vector>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 
 int main()
 {  
     int arr[100];
-    int n,d=INT_MAX;
+    int n=0,d=INT_MAX;
     cin >> n;
-    
-for (int i=0;i<n;i++)
+
+for (int i=0;i<n;++i)
     {   cin>>arr[i];
-    sort(arr,arr+n);
-        d=min(d,abs(arr[i-1] - arr[i]));
+        sort(arr,arr+n);
+        }
+
+for (int i=0;i<n;++i)
+    {   d=std::min(d,abs(arr[i-1] - arr[i]));
+        
     }
     cout<<abs(d);
     return 0;
