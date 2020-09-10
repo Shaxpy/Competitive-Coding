5ea115f7633975f3386bdad16736719b714d7a87
Shashwat Pandey <39722327+Shaxpy@users.noreply.github.com>
2020-09-10 23:46:38 +0530

    Update Pointer.cpp

--- Algorithms Warmup/C++/Pointer.cpp
+++ Algorithms Warmup/C++/Pointer.cpp
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+#include <cstdlib>
+using namespace std;
+void update(int *a,int *b) {
+    // Complete this function  
+      int l=*a;
+      *a=(*a)+(*b);
+      *b=abs(l-(*b));
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
